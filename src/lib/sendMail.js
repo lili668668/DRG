f5cc3f202567646b3c09dba6c9eb3d7b8b13f560
Tzu-Yin Hong <lili668668@gmail.com>
2018-02-25 23:09:07 +0800

    Add send mail

---
+++ src/lib/sendMail.js
@@ -0,0 +1,17 @@
+const nodemailer = require('nodemailer')
+const showdown = require('showdown')
+const markdownTemplate = require('./markdownTemplate.js')
+const config = require('../data/config.json')
+
+
+module.exports = (report) => {
+  const converter = new showdown.Converter()
+  const transporter = nodemailer.createTransport(config.auth_config)
+  const year = (new Date()).getFullYear()
+  const month = ((new Date()).getMonth() + 1).toFixed(0).padStart(2, '0')
+  const date = ((new Date()).getDate()).toFixed(0).padStart(2, '0')
+  const subject = `Daily report from Lily, ${year}${month}${date}`
+  const html = converter.makeHtml(markdownTemplate.getMarkdown(report))
+  let mail_config = Object.assign({}, config.mail_config, { subject, html })
+  transporter.sendMail(mail_config)
+}
