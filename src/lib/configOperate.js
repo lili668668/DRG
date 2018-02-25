0b807ab851d13c4bd8acf9a1cdd2272a1d5d4f68
Tzu-Yin Hong <lili668668@gmail.com>
2018-02-25 23:07:54 +0800

    Fixed no directory problem

--- src/lib/configOperate.js
+++ src/lib/configOperate.js
@@ -2,12 +2,14 @@ const fs = require('fs')
 const constant = require('../data/constant.json')
 
 const save_path = constant.root_path + constant.save_path + constant.save_file
+const save_directory = constant.root_path + constant.save_path
 
 const getConfig = () => {
   return JSON.parse(fs.readFileSync(save_path))
 }
 
 const setConfig = (report) => {
+  if (!fs.existsSync(save_directory)) fs.mkdirSync(save_directory)
   fs.writeFileSync(save_path, JSON.stringify(report))
 }
 
